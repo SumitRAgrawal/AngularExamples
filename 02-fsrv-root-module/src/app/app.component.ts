7be455d0e2ed8efe44a8b60c79ac90b26533ba9b
Sumit Agrawal <sumit.r.agrawal@hotmail.com>
2018-11-20 23:30:26 +0530

    root module

---
+++ 02-fsrv-root-module/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div>
+    Hello Fiserv
+  </div>
+  `
+})
+
+export class AppComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
