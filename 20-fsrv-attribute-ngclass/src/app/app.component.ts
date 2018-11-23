2c3b2b11458cd494473e799a6300c962fd059477
Sumit Agrawal <sumit.r.agrawal@hotmail.com>
2018-11-23 08:33:20 +0530

    :fire: StackBlitz

--- 20-fsrv-attribute-ngclass/src/app/app.component.ts
+++ 20-fsrv-attribute-ngclass/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles : [
+    '.data {background-color:red }'
+  ],
+  template: `
+  <div class="data">
+    Div 1
+  </div>
+  <div [class.data]="data">
+    Div 2
+  </div>
+  <div [ngClass]="{
+    'data': data,
+    'code': !data
+  }">
+  Div 3
+</div>
+  `
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  data = false;
+  constructor() { }
+
+  ngOnInit() { }
 }
