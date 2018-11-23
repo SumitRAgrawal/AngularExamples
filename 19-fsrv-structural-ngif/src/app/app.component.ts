3c94518bdccc4831f8c2b6430594b8ef12b9e053
Sumit Agrawal <sumit.r.agrawal@hotmail.com>
2018-11-23 07:51:02 +0530

    ng if

--- 19-fsrv-structural-ngif/src/app/app.component.ts
+++ 19-fsrv-structural-ngif/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div>
+    <input type="text" [value]="name" (input)= handleInput($event) >
+  </div>
+
+  <div *ngIf="name else elseblock">
+  Name is : {{name}}
+  </div>
+
+  <ng-template #elseblock>
+    Please enter name
+  </ng-template>
+  `
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  name = '';
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  handleInput($event) {
+    this.name = $event.target.value;
+  }
 }
